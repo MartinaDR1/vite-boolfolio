8b6cd81ba27cacd718727fdc76a74a82f94d7d45
MartinaDR1 <derosemartina933@gmail.com>
2023-06-13 18:06:45 +0200

    add cards

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios'
+
+    export default {
+        data(){
+            return{
+                base_API:'http://127.0.0.1:8000/',
+                projects_path: 'api/projects',
+                projects:[],
+            }
+        },      
+        methods: {
+            getProjects(url){
+                axios
+                .get (url)
+                .then (response=> {
+                    this.projects= response.data.projects
+                    console.log(this.projects);
+                })
+                .catch(error=>{
+                    console.error(error)
+                })
+            }
+        },
+        mounted(){
+            const url= this.base_API + this.projects_path
+            console.log(url);
+            this.getProjects(url)
+        }
+    }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+   <div class="container">
+        <div class="row row-cols-sm-1 row-cols-md-3 row-cols-xl-4">
+            <div class="card" v-for="project in projects">
+                <img class="card-img-top" :src="base_API + storage/ + project.project_image" alt="Title">
+                <div class="card-body">
+                    <h4 class="card-title">{{project.title }}</h4>
+                    <p class="card-text">{{project.description }}</p>
+                </div>
+            </div>
+        </div>
+   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style lang="scss" scoped>
+@use './styles/general.scss';
 </style>
